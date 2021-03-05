b93d4addf42c1d1e2716968f7610b1892b704d63
Junhyung-Choi <junhyung9571@naver.com>
2021-03-05 21:43:12 +0900

    SOLVE 11053

---
+++ Mar/week_1/11053.py
@@ -0,0 +1,12 @@
+#BOJ 11053 - 가장 긴 증가하는 부분 수열
+n = int(input())
+data = list(map(int,input().split()))
+dp = [1] * n
+for i in range(1,n):
+    under_max = -1
+    index = 0
+    for j in range(0,i):
+        if data[j] < data[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+#print(dp)
+print(max(dp))
