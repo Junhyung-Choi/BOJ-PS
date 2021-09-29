13c8c3e12641f1c397b8623cfe29e5b0668d8da0
Junhyung-Choi <junhyung9571@naver.com>
2021-09-29 13:00:31 +0900

    UPDATE 1094

--- Sep/1094.cpp
+++ Sep/1094.cpp
@@ -2,23 +2,14 @@
 #include <string>
 using namespace std;
 
-string toBinary(int n)
-{
-    string r;
-    while (n != 0)
-    {
-        r += (n%2==0?"0":"1");
-        n /= 2;
-    }
-    return r;
-}
-
 int main(void)
 {
     int x,cnt = 0;
     cin >> x;
-    string bin = toBinary(x);
-    for(int i=0; i<bin.size(); i++)
-        if (bin.at(i) == '1') cnt++;
+    while (x != 0)
+    {
+        cnt += (x%2==0?0:1);
+        x/=2;
+    }
     cout << cnt << endl;
 }
